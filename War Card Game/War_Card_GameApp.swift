72d5bac77e01002ddc88af636f0b46378b48b5d2
Manio Khan <maniokhan2002@gmail.com>
2025-12-12 09:19:31 +0500

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Manio Khan on 12/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
